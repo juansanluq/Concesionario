8e791f2b850e43430061179c0c1b15f445dce7ea
juansanluq <juansanluq@gmail.com>
2018-11-29 22:48:41 +0100

    Implementación de los estilo

--- app/src/main/java/com/example/juan/concesionario/Principal.java
+++ app/src/main/java/com/example/juan/concesionario/Principal.java
@@ -5,21 +5,12 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class Principal extends AppCompatActivity {
-    private ImageView imgv;
-    private Vehiculo vehiculoMostrar;
     private ConectorBBDD baseDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
-        imgv = findViewById(R.id.imgv);
-        baseDatos = new ConectorBBDD(this);
-
-        vehiculoMostrar = baseDatos.recuperarVehiculo(2);
-        imgv.setImageBitmap(vehiculoMostrar.getImagenBitmap());
-
 
     }
 }
